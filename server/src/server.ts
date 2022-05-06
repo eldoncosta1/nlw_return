7ceb4916ee7423416ac2affaab15af23b5452dd4
Eldon Costa <eldoncosta1@gmail.com>
2022-05-06 18:36:33 -0300

    deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("HTTP server running on port 3333!");
+  console.log(`HTTP server running on port ${process.env.PORT}!`);
 });
